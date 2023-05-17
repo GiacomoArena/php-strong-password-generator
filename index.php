9789f122374f76c28cb71c1318298f86de44831b
GiacomoArena <giacomoarena28@gmail.com>
2023-05-17 15:47:49 +0200

    update commit

--- index.php
+++ index.php
@@ -12,9 +12,23 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- bootstrap -->
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap-utilities.min.css' integrity='sha512-DEGBrwaCF4lkKzMKNwt8Qe/V54bmJctk7I1HyfINGAIugDvsdBeuWzAWZmXAmm49P6EBfl/OeUM01U3r7cW4jg==' crossorigin='anonymous'/><link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous'/>
+  <!-- // bootstrap -->
   <title>PHP Strong Password Generator</title>
 </head>
 <body>
-  <h1>exercise</h1>
+  
+<div class="container my-5">
+
+  <form action="index.php">
+    <label for="quantity">Scegli la lunghezza della password:</label>
+    <input type="number" id="quantity" name="quantity" min="1" max="20">
+    <button type="submit" class="btn btn-success ms-3">Submit</button>
+  </form>
+
+</div>
+
+
 </body>
 </html>
